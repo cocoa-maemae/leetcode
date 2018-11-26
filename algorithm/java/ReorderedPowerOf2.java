b1cfcc92624e0f814ddc58cd7d8deb32db9ae784
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2018-11-26 19:04:37 +0100

    remove tab

--- algorithm/java/ReorderedPowerOf2.java
+++ algorithm/java/ReorderedPowerOf2.java
@@ -6,15 +6,15 @@ class Solution {
     /**
      * check whether two numbers have the same digits by comparing the count of their digits.
      * For example, 338 and 833 have the same digits because they both have exactly two 3's and one 8.
-	 * Time Complexity:  O(log^2N)
-	 * Space Complexity: O(logN) 
+     * Time Complexity:  O(log^2N)
+     * Space Complexity: O(logN) 
      **/
     public boolean reorderedPowerOf2(int N) {
         int[] A = count(N);
         for (int i = 0; i < 31; ++i) {
             // compare with 1 which shifts i bit to left.
-			// 1 which shifts i bit to left means power of 2
-			System.out.println(Arrays.toString(count(1 << i)));
+            // 1 which shifts i bit to left means power of 2
+            System.out.println(Arrays.toString(count(1 << i)));
             if (Arrays.equals(A, count(1 << i))) {
                 return true;
             }
@@ -39,20 +39,20 @@ class Solution {
 
 public class ReorderedPowerOf2 {
     public static String booleanToString(boolean input) {
-		return input ? "True" : "False";
+        return input ? "True" : "False";
     }
 
     /**
-	 * Starting with a positive integer N, we reorder the digits in any order (including the original order) such that the leading digit is not zero.
-	 * Return true if and only if we can do this in a way such that the resulting number is a power of 2.
-	 * Example 1:
-	 * Input: 1
-	 * Output: true
-	 *
-	 * Example 2:
-	 * Input: 10
-	 * Output: false
-	 *
+     * Starting with a positive integer N, we reorder the digits in any order (including the original order) such that the leading digit is not zero.
+     * Return true if and only if we can do this in a way such that the resulting number is a power of 2.
+     * Example 1:
+     * Input: 1
+     * Output: true
+     *
+     * Example 2:
+     * Input: 10
+     * Output: false
+     *
      * Example 3:
      * Input: 16
      * Output: true
@@ -69,14 +69,14 @@ public class ReorderedPowerOf2 {
      * 1 <= N <= 10^9
     **/
     public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		String line;
-		while ((line = in.readLine()) != null) {
-			int N = Integer.parseInt(line);
-			boolean ret = new Solution().reorderedPowerOf2(N);
-			String out = booleanToString(ret);
-			System.out.print(out + "\n");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int N = Integer.parseInt(line);
+            boolean ret = new Solution().reorderedPowerOf2(N);
+            String out = booleanToString(ret);
+            System.out.print(out + "\n");
             break;
-		}
+        }
     }
 }
