b2bfaf165addfa8accf48294e98b0912a387b67d
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2018-12-30 02:57:16 +0100

    fix logic

--- algorithm/java/LargestTimeFromDigits.java
+++ algorithm/java/LargestTimeFromDigits.java
@@ -3,41 +3,25 @@ import java.io.*;
 import java.util.Arrays;
 
 class Solution {
-    /**
-     * 
-     * Time Complexity: O(1)
-     * Space Complexity: O(1)
-     **/
-    String ans;
-    public String largestTimeFromDigits(int[] A) {
-        ans = "";
-        check(A[0], A[1], A[2], A[3]);
-        check(A[0], A[2], A[1], A[3]);
-        check(A[0], A[3], A[1], A[2]);
-        check(A[1], A[2], A[0], A[3]);
-        check(A[1], A[3], A[0], A[2]);
-        check(A[2], A[3], A[0], A[1]);
-        return ans;
-    }
-
-    // For this choice of hour and minute digits, update the answer
-    private void check(int h1, int h2, int m1, int m2) {
-        String hour = best(h1, h2, 24);
-        String minute = best(m1, m2, 60);
-        if (hour.isEmpty() || minute.isEmpty()) {
-            return;
+    public String largestTimeFromDigits(int[] a) {
+        LinkedList<String> q = new LinkedList<>();
+        q.add("");
+        for (int n : a) {
+            for (int size = q.size(); size > 0; size--) {
+                String s = q.poll();
+                for (int i = 0; i <= s.length(); i++) {
+                    q.add(s.substring(0, i) + n + s.substring(i));
+                }
+            }
         }
-        String cand = hour + ":" + minute;
-        if (cand.compareTo(ans) > 0) {
-            ans = cand;
+        String largest = "";
+        for (String s : q) {
+            s = s.substring(0, 2) + ":" + s.substring(2);
+            if (s.charAt(3) < '6' && s.compareTo("24:00") < 0 && s.compareTo(largest) > 0) {
+                largest = s;
+            }
         }
-    }
-
-    // What is the largest string less than limit from these 2 digits?
-    private String best(int d1, int d2, int limit) {
-        int ans = Math.max(10*d1 + d2 < limit ? 10*d1 + d2 : -1,
-                           10*d2 + d1 < limit ? 10*d2 + d1 : -1);
-        return ans >= 0 ? String.format("%02d", ans) : "";
+        return largest;
     }
 }
 
