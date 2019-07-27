ccc6a298103428a958e8233c44616fae29a39fe8
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2019-07-27 22:37:06 +0200

    CombinationSum2

--- algorithm/java/CombinationSum2.java
+++ algorithm/java/CombinationSum2.java
@@ -11,18 +11,18 @@ class Solution {
     }
 
     private void backtrack(
-            List<List<Integer>> list, List<Integer> tempList, int[] cand, int remain, int start) {
+            List<List<Integer>> list, List<Integer> tmp, int[] cand, int remain, int start) {
 
         if (remain < 0) return;
         /** no solution */
-        else if (remain == 0) list.add(new ArrayList<>(tempList));
+        else if (remain == 0) list.add(new ArrayList<>(tmp));
         else {
             for (int i = start; i < cand.length; i++) {
                 if (i > start && cand[i] == cand[i - 1]) continue;
                 /** skip duplicates */
-                tempList.add(cand[i]);
-                backtrack(list, tempList, cand, remain - cand[i], i + 1);
-                tempList.remove(tempList.size() - 1);
+                tmp.add(cand[i]);
+                backtrack(list, tmp, cand, remain - cand[i], i + 1);
+                tmp.remove(tmp.size() - 1);
             }
         }
     }
