6ca536a25d00800edcfd39a443e57f8b05c988ed
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2019-07-27 23:05:33 +0200

    add JumpGame

---
+++ algorithm/java/JumpGame.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > max) {
+                return false;
+            }
+            max = Math.max(nums[i] + i, max);
+        }
+        return true;
+    }
+}
+
+public class JumpGame {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for (int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static String booleanToString(boolean input) {
+        return input ? "True" : "False";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] nums = stringToIntegerArray(line);
+            boolean ret = new Solution().canJump(nums);
+            String out = booleanToString(ret);
+            System.out.print(out + "\n");
+            break;
+        }
+    }
+}
