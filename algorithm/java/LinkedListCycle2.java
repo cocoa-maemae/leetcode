3e285a367709327de5229ca46db23fde4bb5c53f
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2020-02-02 01:30:34 +0100

    add LinkedListCycle2

---
+++ algorithm/java/LinkedListCycle2.java
@@ -0,0 +1,106 @@
+:a!
+/* -----------------------------------
+ *  WARNING:
+ * -----------------------------------
+ *  Your code may fail to compile
+ *  because it contains public class
+ *  declarations.
+ *  To fix this, please remove the
+ *  "public" keyword from your class
+ *  declarations.
+ */
+
+/**
+ * Definition for singly-linked list. class ListNode { int val; ListNode next; ListNode(int x) { val
+ * = x; next = null; } }
+ */
+class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+        this.val = x;
+        this.next = null;
+    }
+}
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            // slow pointer traveled exactly full cycle when it meets fast pointer
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                //  start another slow pointer at head node, and move both pointers until they meet at the beginning of a cycle.
+                ListNode slow2 = head;
+                while (slow != slow2) {
+                    slow = slow.next;
+                    slow2 = slow2.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
+
+public class LinkedListCycle2 {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+          return new int[0];
+        }
+    
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+    
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+    
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for(int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+    
+    public static String listNodeToString(ListNode node) {
+        if (node == null) {
+            return "[]";
+        }
+    
+        String result = "";
+        while (node != null) {
+            result += Integer.toString(node.val) + ", ";
+            node = node.next;
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            ListNode head = stringToListNode(line);
+            line = in.readLine();
+            int pos = Integer.parseInt(line);
+            ListNode ret = new Solution().detectCycle(head, pos);
+            String out = listNodeToString(ret);
+            System.out.print(out + "\n");
+            break;
+        }
+    }
+}
