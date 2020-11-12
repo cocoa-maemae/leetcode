5724f5d18c011068f0cb16c312e1fd451b245301
tomoki.maeda <tomoki.maeda@brainpad.co.jp>
2020-11-12 22:47:28 +0900

    add Nth Highest Salary

---
+++ database/mysql/NthHighestSalary.sql
@@ -0,0 +1,9 @@
+iCREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE M INT;
+SET M = N - 1;
+  RETURN (
+      SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT M, 1
+      
+  );
+END
