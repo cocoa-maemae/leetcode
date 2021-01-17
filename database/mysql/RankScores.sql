cfa99b5c010267db3e5fcc19efd7c99d7b4c2213
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2021-01-18 01:15:06 +0900

    fix

--- database/mysql/RankScores.sql
+++ database/mysql/RankScores.sql
@@ -1,9 +1,9 @@
-SELECT s.Score, COUNT(s2.Score) AS 'Rank' 
-FROM Scores s,
-(SELECT DISTINCT Score FROM Scores) s2
-WHERE s.Score <= s2.Score
-GROUP BY s.Id 
-ORDER BY s.Score DESC;
+SELECT s1.Score, COUNT(s2.Score) AS 'Rank' 
+FROM Scores AS s1,
+(SELECT DISTINCT Score FROM Scores) AS s2
+WHERE s1.Score <= s2.Score
+GROUP BY s1.Id 
+ORDER BY s1.Score DESC;
 
 
 # See https://leetcode.com/problems/rank-scores/discuss/456610/MySQL-Two-Simple-Solutions-and-Explanations-for-Beginners
